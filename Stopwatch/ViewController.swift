c8ba220858c3c2686e3d3db4fccd4a330aa9b1da
GuillaumeOcculy <occuly.guillaume@gmail.com>
2016-01-04 09:53:22 +0100

    Add chrono

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var time = 0
+    var timer = NSTimer()
+    func updateTime(){
+        time++
+        text.text = String(time)
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func rewind(sender: AnyObject) {
+        time = 0
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        time = 0
+        text.text = "0"
+        timer.invalidate()
+    }
+    
+
+    @IBOutlet var text: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
